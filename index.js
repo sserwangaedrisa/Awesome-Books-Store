13d7782e5c9e355c1e08997b24f9dd13b9ceb945
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-09-14 12:59:42 +0300

    Modified the index.js file to have required functions and storage of the localStorage

--- index.js
+++ index.js
@@ -2,22 +2,46 @@ const title = document.getElementById('Title');
 const author = document.getElementById('Author');
 const addButton = document.getElementById('AddButton');
 const booksSection = document.getElementById('Books-section');
+const addedBooks = [];
 
-const keys = Object.keys(localStorage);
-if (localStorage.length > 0) {
-  keys.forEach((key) => {
+const addingBooksFunction = () => {
+  if (title.value.length !== 0) {
+    const addedBooks = JSON.parse(localStorage.getItem('Added books'));
+    addedBooks.push({ Title: title.value, Author: author.value });
+    localStorage.setItem('Added books', JSON.stringify(addedBooks));
+  }
+};
+
+const cleaningEntries = () => {
+  title.value = '';
+  author.value = '';
+};
+
+const removingBookFunction = (id) => {
+  const booksArray = JSON.parse(localStorage.getItem('Added books'));
+  const updatedArray = booksArray.filter((book) => {
+    return book.Title !== id;
+  });
+  localStorage.setItem('Added books', JSON.stringify(updatedArray));
+};
+
+if (localStorage.getItem('Added books') === null) {
+  localStorage.setItem('Added books', JSON.stringify(addedBooks));
+} else {
+  const addedBook = JSON.parse(localStorage.getItem('Added books'));
+  addedBook.forEach((book) => {
     const newTitle = document.createElement('p');
-    newTitle.textContent = JSON.parse(localStorage.getItem(key)).Title;
+    newTitle.textContent = book.Title;
     const newAuthor = document.createElement('p');
-    newAuthor.textContent = JSON.parse(localStorage.getItem(key)).Author;
+    newAuthor.textContent = book.Author;
     const horizontalLine = document.createElement('hr');
     const removeBtn = document.createElement('button');
     removeBtn.textContent = 'Remove';
     removeBtn.classList.add('Remove');
-    removeBtn.setAttribute('id', key);
+    removeBtn.setAttribute('id', book.Title);
 
-    removeBtn.addEventListener('click', () => {
-      localStorage.removeItem(removeBtn.id);
+    removeBtn.addEventListener('click', (event) => {
+      removingBookFunction(event.target.id);
       window.location.reload();
     });
 
@@ -30,34 +54,8 @@ if (localStorage.length > 0) {
 
 addButton.addEventListener('click', () => {
   if (title.value.length !== 0) {
-    const inputs = {
-      Title: title.value,
-      Author: author.value,
-    };
-
-    localStorage.setItem(title.value, JSON.stringify(inputs));
-    const newTitle = document.createElement('p');
-    newTitle.textContent = JSON.parse(localStorage.getItem(title.value)).Title;
-    const newAuthor = document.createElement('p');
-    newAuthor.textContent = JSON.parse(
-      localStorage.getItem(title.value)
-    ).Author;
-    const horizontalLine = document.createElement('hr');
-    const removeBtn = document.createElement('button');
-    removeBtn.textContent = 'Remove';
-    removeBtn.classList.add('Remove');
-    removeBtn.setAttribute('id', title.value);
-
-    removeBtn.addEventListener('click', () => {
-      localStorage.removeItem(removeBtn.id);
-      window.location.reload();
-    });
-
-    booksSection.appendChild(newTitle);
-    booksSection.appendChild(newAuthor);
-    booksSection.appendChild(removeBtn);
-    booksSection.appendChild(horizontalLine);
+    addingBooksFunction();
   }
-  title.value = '';
-  author.value = '';
+  cleaningEntries();
+  window.location.reload();
 });
